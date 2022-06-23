fd43c517521afaacb987d0685dafaacb48d11c56
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2022-06-23 14:19:33 -0400

    Added necessary punctuation

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList = [
-  { name: 'Dragonite', height: 7, types: ['Dragon', 'Flying'] }
-  { name: 'Tyranitar', height: 6, types: ['Rock', 'Dark'] }
+  { name: 'Dragonite', height: 7, types: ['Dragon', 'Flying'] },
+  { name: 'Tyranitar', height: 6, types: ['Rock', 'Dark'] },
   { name: 'Scizor', height: 6, types: ['Bug', 'Steel'] }
-]
+];
