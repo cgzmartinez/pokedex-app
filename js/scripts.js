591875d3aa98fdf1de29c81fd452c11c902d44a0
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2022-06-27 11:17:04 -0400

    Added loop and conditionals to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,29 @@
 let pokemonList = [
-  { name: 'Dragonite', height: 7, types: ['Dragon', 'Flying'] },
-  { name: 'Tyranitar', height: 6, types: ['Rock', 'Dark'] },
-  { name: 'Scizor', height: 6, types: ['Bug', 'Steel'] }
+  {
+    name: 'Dragonite',
+    height: 7,
+    types: ['Dragon', 'Flying']
+  },
+  {
+    name: 'Tyranitar',
+    height: 6,
+    types: ['Rock', 'Dark']
+  },
+  {
+    name: 'Scizor',
+    height: 5,
+    types: ['Bug', 'Steel']
+  }
 ];
+
+for (let i=0; i<pokemonList.length; i++){
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ').' );
+
+    if(pokemonList[i].height > 6 ) {
+        document.write(' Wow, that\'s big!'+ '<br>');
+    } else if(pokemonList[i].height <7 && pokemonList[i].height >5) {
+        document.write(' That\'s average.' + '<br>');
+    } else {
+        document.write(' That\'s pretty small.' + '<br>');
+    }
+}
