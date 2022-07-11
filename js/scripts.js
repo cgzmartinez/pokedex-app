a40dfaa41550c19cd8143a2b795039609189e3e4
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2022-07-11 00:30:56 -0400

    Added IIFE to scripts.js
    Wrapped the array in IIFE and made notes. Some superficial changes to code was done as well.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,25 @@
-let pokemonList = [
-  {
-    name: 'Dragonite',
-    height: 7,
-    types: ['Dragon', 'Flying']
-  },
-  {
-    name: 'Tyranitar',
-    height: 6,
-    types: ['Rock', 'Dark']
-  },
-  {
-    name: 'Scizor',
-    height: 5,
-    types: ['Bug', 'Steel']
+let pokemonRepository = (function() {
+// wrapping pokemonList array in IIFE.
+
+  let pokemonList = [
+    { name: 'Dragonite', height: 7, types: ['Dragon', 'Flying']},
+    { name: 'Tyranitar', height: 6, types: ['Rock', 'Dark']},
+    { name: 'Scizor', height: 5, types: ['Bug', 'Steel']}
+  ];
+// Changed layout of array to be more concise.
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
-];
+  function getAll() {
+    return pokemonList;
+  }
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+// End of IIFE wrap.
 
 for (let i=0; i<pokemonList.length; i++){
     document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ').' );
