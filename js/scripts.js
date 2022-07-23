60ad96b88a3e5224c2996b24feaff25481e24514
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2022-07-23 07:37:27 -0400

    Added new functions to scripts.js
    Added addListItem function and removed previous forEach function

--- js/scripts.js
+++ js/scripts.js
@@ -9,28 +9,47 @@ let pokemonRepository = (function() {
 // Changed layout of array to be more concise.
 
   function add(pokemon) {
-    pokemonList.push(pokemon);
+    if (
+      typeof pokemon === 'object' &&
+      'name' in pokemon &&
+      'height' in pokemon &&
+      'types' in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log('Pokemon is not correct!');
+    }
   }
+
   function getAll() {
     return pokemonList;
   }
+  function addListItem(pokemon){
+    let pokemonList = document.querySelector(".pokemon-list");
+    // each pokemon assigned to unordered list
+    let listpokemon = document.createElement("li");
+    // assigns pokemon their own button
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+    button.addEventListener('click', function() {
+      showDetails(pokemon);
+    });
+  }
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
+  // retrieves pokemon information and displays on console
   return {
     add: add,
     getAll: getAll,
+    addListItem: addListItem
   };
 })();
 // End of IIFE wrap.
 
-let pokemonList = pokemonRepository.getAll();
-
-pokemonList.forEach(function (pokemon) {
-    if (pokemon.height > 6) {
-        document.write(`${pokemon.name} <br> Height: ${pokemon.height} - Wow, thats's big! <br> <p>`)
-    } else if (pokemon.height < 7 && pokemon.height > 5) {
-        document.write(`${pokemon.name} <br> Height: ${pokemon.height} - That's average. <br> <p>`)
-    } else {
-        document.write(`${pokemon.name} <br> Height: ${pokemon.height} - That's pretty small. <br> <p>`)
-    }
-})
-// ${} didn't work the first few times when using it, look in to that later.
-// Remember that you can use html in js
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+});
