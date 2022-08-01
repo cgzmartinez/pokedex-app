ebdd69a4b0c8b291420807294a9fa452fbd62972
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2022-08-01 02:02:06 -0400

    Added API and new functions to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,16 @@
 let pokemonRepository = (function() {
-// wrapping pokemonList array in IIFE.
 
-  let pokemonList = [
-    { name: 'Dragonite', height: 7, types: ['Dragon', 'Flying']},
-    { name: 'Tyranitar', height: 6, types: ['Rock', 'Dark']},
-    { name: 'Scizor', height: 5, types: ['Bug', 'Steel']}
-  ];
-// Changed layout of array to be more concise.
+    let pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function add(pokemon) {
-    if (
-      typeof pokemon === 'object' &&
-      'name' in pokemon &&
-      'height' in pokemon &&
-      'types' in pokemon
-    ) {
-      pokemonList.push(pokemon);
-    } else {
-      console.log('Pokemon is not correct!');
-    }
+    pokemonList.push(pokemon);
   }
 
   function getAll() {
     return pokemonList;
   }
+
   function addListItem(pokemon){
     let pokemonList = document.querySelector(".pokemon-list");
     // each pokemon assigned to unordered list
@@ -38,18 +25,53 @@ let pokemonRepository = (function() {
       showDetails(pokemon);
     });
   }
+
+  function loadList () {
+    return fetch(apiUrl).then(function(response){
+      return response.json();
+    }).then(function (json){
+      json.results.forEach(function(item){
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e){
+      console.error(e);
+    })
+  }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
   function showDetails(pokemon) {
+    loadDetails(pokemon).then(function () {
     console.log(pokemon);
   }
-  // retrieves pokemon information and displays on console
+
   return {
     add: add,
     getAll: getAll,
-    addListItem: addListItem
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
   };
-})();
-// End of IIFE wrap.
+}) ();
 
+pokemonRepository.loadList().then(function (){
   pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
+  });
 });
