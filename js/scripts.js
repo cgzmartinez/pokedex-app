d7954a75341eb3a6c2a3ba002a79c798ebfbd09c
cgzmartinez <105771374+cgzmartinez@users.noreply.github.com>
2022-08-01 02:36:51 -0400

    Fixed syntax errors throughout scripts.js
    The buttons weren't showing up due to syntax errors.

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,14 @@ let pokemonRepository = (function() {
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function add(pokemon) {
-    pokemonList.push(pokemon);
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
+    }
   }
 
   function getAll() {
@@ -14,14 +21,14 @@ let pokemonRepository = (function() {
   function addListItem(pokemon){
     let pokemonList = document.querySelector(".pokemon-list");
     // each pokemon assigned to unordered list
-    let listpokemon = document.createElement("li");
+    let listPokemon = document.createElement("li");
     // assigns pokemon their own button
     let button = document.createElement("button");
     button.innerText = pokemon.name;
     button.classList.add("button-class");
-    listpokemon.appendChild(button);
-    pokemonList.appendChild(listpokemon);
-    button.addEventListener('click', function() {
+    listPokemon.appendChild(button);
+    pokemonList.appendChild(listPokemon);
+    button.addEventListener('click', function(event) {
       showDetails(pokemon);
     });
   }
@@ -56,8 +63,9 @@ let pokemonRepository = (function() {
   }
 
   function showDetails(pokemon) {
-    loadDetails(pokemon).then(function () {
-    console.log(pokemon);
+    pokemonRepository.loadDetails(item).then(function () {
+      console.log(pokemon);
+    });
   }
 
   return {
@@ -68,7 +76,7 @@ let pokemonRepository = (function() {
     loadDetails: loadDetails,
     showDetails: showDetails
   };
-}) ();
+})();
 
 pokemonRepository.loadList().then(function (){
   pokemonRepository.getAll().forEach(function (pokemon) {
